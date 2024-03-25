9f5923e0a03a9779fd5e3dccaf474ae4ae298896
Jawad Saeed <113991148+jawadsaeed126@users.noreply.github.com>
2024-03-25 17:12:40 +0500

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, from flask application'
+    return 'Hello, from flask application...How are you'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
